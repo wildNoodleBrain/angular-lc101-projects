91a80f8917c2cf0a87a6cb9049094adbb20b54cf
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-07-19 16:53:26 -0500

    Done with Lesson 1 Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "Emily's Random Images";
+  image1 = 'https://littleblacksubs.files.wordpress.com/2021/01/7317d471f7fadf6918662555f9369252.jpg';
+  image2 = 'https://i.imgur.com/gGg1AK0.jpeg';
+  image3 = 'https://i.imgur.com/hZ85NZi.jpeg';
 
   constructor() { }
 
