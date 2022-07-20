6849789248e7952b7a42854c6cfd6d5b740377f3
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-07-20 16:42:27 -0500

    Next toDo 30.7.4. Sidekick Image Column

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,9 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
-}
+
+   addMovie (newTitle: string) {
+      if(!this.movies.includes(newTitle)){
+         this.movies.push(newTitle);
+      }
+   }}
